7889100d605eeb9a33c3e6cb675e7da2d78804f3
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-09-16 11:52:17 +0200

    Remap leader to Space

--- vim/.config/nvim/lua/maps.lua
+++ vim/.config/nvim/lua/maps.lua
@@ -1,5 +1,7 @@
 local keymap = vim.keymap
 
+vim.g.mapleader = ' '
+
 keymap.set('n', 'x', '"_x')
 
 -- Increment/decrement
@@ -21,7 +23,7 @@ keymap.set('n', 'te', ':tabedit')
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
+-- keymap.set('n', '<Space>', '<C-w>w')
 keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
