b9c5cc3f302c13e6dbe2451768785f89b457c0d7
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-09-22 06:25:26 +0200

    Add OmniSharp support for C#.

--- vim/.config/nvim/plugin/lspconfig.lua
+++ vim/.config/nvim/plugin/lspconfig.lua
@@ -5,6 +5,12 @@ if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
 
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+local is_mac = has "macunix"                                                                                                                                                                                                   
+local is_win = has "win32"
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -93,6 +99,54 @@ nvim_lsp.sumneko_lua.setup {
 }
 
 nvim_lsp.tailwindcss.setup {}
+
+local omni_cmd
+if is_mac then
+  omni_cmd = { "dotnet", "/Users/tiboemv/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll" }
+end
+if is_win then
+  omni_cmd = { "dotnet", "/mnt/c/Users/Emil/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll" }
+end
+
+nvim_lsp.omnisharp.setup {
+  on_attach = on_attach,
+  cmd = omni_cmd,
+
+  -- Enables support for reading code style, naming convention and analyzer
+  -- settings from .editorconfig.
+  enable_editorconfig_support = true,
+
+  -- If true, MSBuild project system will only load projects for files that
+  -- were opened in the editor. This setting is useful for big C# codebases
+  -- and allows for faster initialization of code navigation features only
+  -- for projects that are relevant to code that is being edited. With this
+  -- setting enabled OmniSharp may load fewer projects and may thus display
+  -- incomplete reference lists for symbols.
+  enable_ms_build_load_projects_on_demand = false,
+
+  -- Enables support for roslyn analyzers, code fixes and rulesets.
+  enable_roslyn_analyzers = false,
+
+  -- Specifies whether 'using' directives should be grouped and sorted during
+  -- document formatting.
+  organize_imports_on_format = false,
+
+  -- Enables support for showing unimported types and unimported extension
+  -- methods in completion lists. When committed, the appropriate using
+  -- directive will be added at the top of the current file. This option can
+  -- have a negative impact on initial completion responsiveness,
+  -- particularly for the first few completion sessions after opening a
+  -- solution.
+  enable_import_completion = false,
+
+  -- Specifies whether to include preview versions of the .NET SDK when
+  -- determining which version to use for project loading.
+  sdk_include_prereleases = true,
+
+  -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+  -- true
+  analyze_open_documents_only = false,
+}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
