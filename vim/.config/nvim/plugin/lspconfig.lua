8c07c61b653e619b774dba60514e2a1f0ecd53bd
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-12-09 22:22:45 +0100

    Neovim(lsp): make vue language server work.

--- vim/.config/nvim/plugin/lspconfig.lua
+++ vim/.config/nvim/plugin/lspconfig.lua
@@ -100,6 +100,37 @@ nvim_lsp.sumneko_lua.setup {
 
 nvim_lsp.tailwindcss.setup {}
 
+local util = require('lspconfig.util')
+local function get_typescript_server_path(root_dir)
+
+  local global_ts = '/Users/tiboemv/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib'
+  local found_ts = ''
+  local function check_dir(path)
+    found_ts =  util.path.join(path, 'node_modules', 'typescript', 'lib')
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+nvim_lsp.volar.setup {
+  init_options = {
+    typescript = {
+      tsdk = '',
+    },
+  },
+  on_new_config = function (new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 local omni_cmd
 if is_mac then
   omni_cmd = { "dotnet", "/Users/tiboemv/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll" }
