755634e9703125ae30088b21ec6e260a40ffa1f3
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-12-11 23:33:22 +0100

    Neovim(lsp): fix signature help.

--- vim/.config/nvim/plugin/lspconfig.lua
+++ vim/.config/nvim/plugin/lspconfig.lua
@@ -29,6 +29,7 @@ local on_attach = function(client, bufnr)
   --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 end
 
 protocol.CompletionItemKind = {
