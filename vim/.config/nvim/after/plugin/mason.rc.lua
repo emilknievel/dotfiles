be8c0d22c0721ddba8237d79ca6828ebc3e9d8bc
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-12-13 20:28:12 +0100

    Neovim(mason): rename to make clearer.

--- vim/.config/nvim/after/plugin/mason.rc.lua
+++ vim/.config/nvim/after/plugin/mason.rc.lua
@@ -1,6 +1,6 @@
 local status, mason = pcall(require, "mason")
 if (not status) then return end
-local status2, lspconfig = pcall(require, "mason-lspconfig")
+local status2, mason_lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 local status3, mason_null_ls = pcall(require, "mason-null-ls")
 if (not status3) then return end
@@ -15,7 +15,7 @@ mason.setup({
 
 })
 
-lspconfig.setup {
+mason_lspconfig.setup {
   ensure_installed = {
     "awk_ls",
     "bashls",
