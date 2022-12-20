c98cfd5f88cf3859b3edc27c97299076eee6cd14
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-12-20 23:45:47 +0100

    Neovim: Revert to old LSP config.

---
+++ vim/.config/nvim/plugin/lspconfig.lua
@@ -0,0 +1,242 @@
+--vim.lsp.set_log_level("debug")
+
+local status, nvim_lsp = pcall(require, 'lspconfig')
+if not status then return end
+
+local protocol = require('vim.lsp.protocol')
+
+local has = function(x) return vim.fn.has(x) == 1 end
+local is_mac = has('macunix')
+local is_win = has('win32')
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap = true, silent = true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap(
+    'i',
+    '<C-k>',
+    '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+    opts
+  )
+end
+
+protocol.CompletionItemKind = {
+  '', -- Text
+  '', -- Method
+  '', -- Function
+  '', -- Constructor
+  '', -- Field
+  '', -- Variable
+  '', -- Class
+  'ﰮ', -- Interface
+  '', -- Module
+  '', -- Property
+  '', -- Unit
+  '', -- Value
+  '', -- Enum
+  '', -- Keyword
+  '﬌', -- Snippet
+  '', -- Color
+  '', -- File
+  '', -- Reference
+  '', -- Folder
+  '', -- EnumMember
+  '', -- Constant
+  '', -- Struct
+  '', -- Event
+  'ﬦ', -- Operator
+  '', -- TypeParameter
+}
+
+-- Set up completion using nvim_cmp with LSP source
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
+  vim.lsp.protocol.make_client_capabilities()
+)
+
+nvim_lsp.awk_ls.setup({})
+
+nvim_lsp.bashls.setup({})
+
+nvim_lsp.cssls.setup({})
+
+nvim_lsp.dockerls.setup({})
+
+nvim_lsp.flow.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+nvim_lsp.gopls.setup({})
+
+nvim_lsp.graphql.setup({})
+
+nvim_lsp.jsonls.setup({})
+
+nvim_lsp.marksman.setup({})
+
+local omni_cmd
+if is_mac then
+  omni_cmd = {
+    'dotnet',
+    '/Users/tiboemv/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll',
+  }
+end
+if is_win then
+  omni_cmd = {
+    'dotnet',
+    '/mnt/c/Users/Emil/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll',
+  }
+end
+
+nvim_lsp.omnisharp.setup({
+  on_attach = on_attach,
+  cmd = omni_cmd,
+
+  -- Enables support for reading code style, naming convention and analyzer
+  -- settings from .editorconfig.
+  enable_editorconfig_support = true,
+
+  -- If true, MSBuild project system will only load projects for files that
+  -- were opened in the editor. This setting is useful for big C# codebases
+  -- and allows for faster initialization of code navigation features only
+  -- for projects that are relevant to code that is being edited. With this
+  -- setting enabled OmniSharp may load fewer projects and may thus display
+  -- incomplete reference lists for symbols.
+  enable_ms_build_load_projects_on_demand = false,
+
+  -- Enables support for roslyn analyzers, code fixes and rulesets.
+  enable_roslyn_analyzers = true,
+
+  -- Specifies whether 'using' directives should be grouped and sorted during
+  -- document formatting.
+  organize_imports_on_format = false,
+
+  -- Enables support for showing unimported types and unimported extension
+  -- methods in completion lists. When committed, the appropriate using
+  -- directive will be added at the top of the current file. This option can
+  -- have a negative impact on initial completion responsiveness,
+  -- particularly for the first few completion sessions after opening a
+  -- solution.
+  enable_import_completion = true,
+
+  -- Specifies whether to include preview versions of the .NET SDK when
+  -- determining which version to use for project loading.
+  sdk_include_prereleases = true,
+
+  -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+  -- true
+  analyze_open_documents_only = false,
+})
+
+nvim_lsp.pylsp.setup({})
+
+nvim_lsp.rust_analyzer.setup({})
+
+nvim_lsp.sourcekit.setup({
+  on_attach = on_attach,
+})
+
+nvim_lsp.sumneko_lua.setup({
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+    },
+  },
+})
+
+nvim_lsp.tailwindcss.setup({})
+
+local util = require('lspconfig.util')
+local function get_typescript_server_path(root_dir)
+  local global_ts =
+    '/Users/tiboemv/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib'
+  local found_ts = ''
+  local function check_dir(path)
+    found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
+    if util.path.exists(found_ts) then return path end
+  end
+
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+nvim_lsp.tsserver.setup({
+  on_attach = on_attach,
+  filetypes = {
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+    'javascript',
+  },
+  cmd = { 'typescript-language-server', '--stdio' },
+  capabilities = capabilities,
+})
+
+nvim_lsp.volar.setup({
+  init_options = {
+    typescript = {
+      tsdk = '',
+    },
+  },
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk =
+      get_typescript_server_path(new_root_dir)
+  end,
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+nvim_lsp.yamlls.setup({})
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = '●' },
+    severity_sort = true,
+  })
+
+-- Diagnostic symbols in the sign column (gutter)
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+end
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',
+  },
+  update_in_insert = true,
+  float = {
+    source = 'always', -- Or "if_many"
+  },
+})
