9e630564da8d58e6cc44feb25782860bf19ca751
Emil Vågstedt <emil.vagstedt@icloud.com>
2022-12-20 06:57:45 +0100

    Neovim: add vim-fugitive.

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -102,6 +102,7 @@ packer.startup(function(use)
   })
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
