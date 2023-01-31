3d0d9cea39660e15f2ddeef01634f531d150a919
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-01-31 06:16:17 +0100

    Neovim: Improve lualine.

--- vim/.config/nvim/after/plugin/lualine.rc.lua
+++ vim/.config/nvim/after/plugin/lualine.rc.lua
@@ -9,33 +9,21 @@ end
 
 lualine.setup {
   options = {
-    icons_enabled = true,
-    -- theme = 'gruvbox-material',
-    -- theme = 'rose-pine',
-    theme = 'catppuccin',
-    -- theme = "tokyonight",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    },
+    theme = "auto",
+    component_separators = "|",
+    section_separators = { left = "", right = "" },
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = {
+      { "mode", separator = { left = "" }, right_padding = 2 },
+    },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { maximize_status, "filename" },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
-    lualine_z = { "location" },
+    lualine_z = {
+      { "location", separator = { right = "" }, left_padding = 2 },
+    },
   },
   inactive_sections = {
     lualine_a = {},
