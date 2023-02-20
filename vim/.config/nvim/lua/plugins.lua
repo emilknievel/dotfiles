640850d98c4884ce18d892d160023c134d03fc7b
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-02-20 21:38:37 +0100

    Neovim: Add whitespace highlight plugin.

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -167,6 +167,25 @@ packer.startup(function(use)
   end
 }
   use "joechrisellis/lsp-format-modifications.nvim"
+  use {
+    "johnfrankmorgan/whitespace.nvim",
+    config = function()
+      require("whitespace-nvim").setup {
+        -- configuration options and their defaults
+
+        -- `highlight` configures which highlight is used to display
+        -- trailing whitespace
+        highlight = "DiffDelete",
+
+        -- `ignored_filetypes` configures which filetypes to ignore when
+        -- displaying trailing whitespace
+        ignored_filetypes = { "TelescopePrompt", "Trouble", "help" },
+      }
+
+      -- remove trailing whitespace with a keybinding
+      -- vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim)
+    end,
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
