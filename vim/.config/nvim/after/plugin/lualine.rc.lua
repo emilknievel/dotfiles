a608b0441ee9372003f2b06cd4d9b5cbde33467f
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-03-06 23:22:36 +0100

    Neovim: Show current lsp in lualine.

--- vim/.config/nvim/after/plugin/lualine.rc.lua
+++ vim/.config/nvim/after/plugin/lualine.rc.lua
@@ -7,6 +7,29 @@ local function maximize_status()
   return vim.t.maximized and "   " or ""
 end
 
+local function getLspName()
+  local msg = "No Active Lsp"
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return "  " .. client.name
+    end
+  end
+  return "  " .. msg
+end
+
+local lsp = {
+  function()
+    return getLspName()
+  end,
+  separator = { left = "", right = "" },
+}
+
 lualine.setup {
   options = {
     theme = "auto",
@@ -20,9 +43,9 @@ lualine.setup {
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { maximize_status, "filename" },
     lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress" },
+    lualine_y = { "progress", "location" },
     lualine_z = {
-      { "location", separator = { right = "" }, left_padding = 2 },
+      lsp
     },
   },
   inactive_sections = {
