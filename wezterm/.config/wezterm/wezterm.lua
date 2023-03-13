16f77c4ed90011adfd6d220c7fb2377151741060
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-03-13 23:26:36 +0100

    Wezterm: Nudge font into perfection.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,8 +12,12 @@ end
 
 return {
   font = wezterm.font "FiraCode Nerd Font",
-  font_size = 10.0,
+  font_size = 12.0,
   freetype_load_flags = "NO_HINTING",
+
+  -- Disable ligatures
+  -- harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
+
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   -- color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
@@ -21,4 +25,5 @@ return {
   default_cursor_style = "BlinkingBlock",
   cursor_blink_ease_in = "Constant",
   cursor_blink_ease_out = "Constant",
+
 }
