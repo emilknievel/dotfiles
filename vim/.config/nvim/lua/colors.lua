67c4b88dc002ab7e4fd2ca325c7665c8132dae10
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-05-05 22:37:39 +0200

    Theme: Rosé pine.

--- vim/.config/nvim/lua/colors.lua
+++ vim/.config/nvim/lua/colors.lua
@@ -28,7 +28,7 @@ require("rose-pine").setup {
   --- @usage 'auto'|'main'|'moon'|'dawn'
   variant = "auto",
   --- @usage 'main'|'moon'|'dawn'
-  dark_variant = "moon",
+  dark_variant = "main",
   bold_vert_split = false,
   dim_nc_background = false,
   disable_background = false,
