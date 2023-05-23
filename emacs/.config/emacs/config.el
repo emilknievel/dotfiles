66d9351d393a491b21c1f9229d7c2640a3cf5823
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-05-23 23:45:04 +0200

    Emacs: Corfu for completion.
    - WIP, need to add some more flavor to it.
    - Changed editor font to AnonymicePro Nerd Font.
    - Fixed column guide.
    - Removed visible bell (annoying on macOS)

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -60,7 +60,7 @@
       apropos-do-all t
       mouse-yank-at-point t
       require-final-newline t
-      visible-bell t
+      ; visible-bell t
       load-prefer-newer t
       backup-by-copying t
       frame-inhibit-implied-resize t
@@ -133,15 +133,18 @@
 
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 
-(add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font 14"))
+(add-to-list 'default-frame-alist '(font . "AnonymicePro Nerd Font 15"))
 
 ;; Render fonts like in iTerm
 ;; Still need to set `defaults write org.gnu.Emacs AppleFontSmoothing -int`
 ;; in the terminal for it to work like intended.
 (setq ns-use-thin-smoothing t)
 
-(setq fill-column 80)
-(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+(defun my/show-column-guide ()
+  (setq display-fill-column-indicator-column 80)
+  (display-fill-column-indicator-mode))
+
+(add-hook 'prog-mode-hook #'my/show-column-guide)
 
 (column-number-mode 1)
 
@@ -171,18 +174,44 @@
   (setq treesit-auto-install 'prompt)
   (global-treesit-auto-mode))
 
-;;(use-package corfu
-;;  :general
-;;  (:keymaps 'corfu-map
-;;   :states 'insert
-;;   "C-n" #'corfu-next
-;;   "C-p" #'corfu-previous
-;;   "<escape>" #'corfu-quit
-;;   "<return>" #'corfu-insert
-;;   "M-d" #'corfu-show-documentation ; rebind to <leader>-d
-;;   "M-l" #'corfu-show-location)     ; rebind to <leader>-l
-;;  :config
-;;  (corfu-global-mode))
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `corfu-exclude-modes'.
+  :init
+  (global-corfu-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
 
 (add-to-list 'auto-mode-alist '("\\.pl?\\'" . prolog-mode))
 
