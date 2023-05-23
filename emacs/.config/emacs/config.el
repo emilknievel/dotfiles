0a01de5ae42c75f49c422782de9f8947bd03bc3a
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-05-23 23:59:52 +0200

    Emacs: VSCode style git gutter.

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -216,3 +216,15 @@
 (add-to-list 'auto-mode-alist '("\\.pl?\\'" . prolog-mode))
 
 (use-package magit)
+
+(use-package git-gutter
+  :hook ((prog-mode . git-gutter-mode)
+         (org-mode . git-gutter-mode))
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
