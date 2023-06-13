1157561266257ed87f3ec864149a723190851c5f
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-06-13 10:03:09 +0200

    vim: rosé  pine.

--- vim/.config/nvim/lua/colors.lua
+++ vim/.config/nvim/lua/colors.lua
@@ -15,15 +15,7 @@ vim.opt.background = "dark"
 --vim.wo.colorcolumn = vim.fn.join(vim.fn.range(81,999),',')
 -- vim.wo.colorcolumn = "81"
 
--- set contrast "hard, medium(default), soft"
--- vim.g.gruvbox_material_background = 'medium'
-
--- for better performance
--- vim.g.gruvbox_material_better_performance = 1
-
--- vim.cmd('colorscheme gruvbox-material')
-
--- Rose pine
+-- Rosé Pine
 require("rose-pine").setup {
   --- @usage 'auto'|'main'|'moon'|'dawn'
   variant = "auto",
@@ -33,7 +25,7 @@ require("rose-pine").setup {
   dim_nc_background = false,
   disable_background = false,
   disable_float_background = false,
-  disable_italics = true,
+  disable_italics = false,
   --- @usage string hex value or named color from rosepinetheme.com/palette
   groups = {
     background = "base",
@@ -69,86 +61,4 @@ require("rose-pine").setup {
   },
 }
 
--- Catppuccin
-require("catppuccin").setup {
-  flavour = "frappe", -- latte, frappe, macchiato, mocha
-  background = {
-    -- :h background
-    light = "latte",
-    dark = "frappe",
-  },
-  transparent_background = false,
-  term_colors = false,
-  dim_inactive = {
-    enabled = false,
-    shade = "dark",
-    percentage = 0.15,
-  },
-  no_italic = true, -- Force no italic
-  no_bold = true, -- Force no bold
-  styles = {
-    comments = { "italic" },
-    conditionals = { "italic" },
-    loops = {},
-    functions = {},
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = {},
-    properties = {},
-    types = {},
-    operators = {},
-  },
-  color_overrides = {},
-  custom_highlights = {},
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true,
-    telescope = true,
-    notify = false,
-    mini = false,
-    lsp_saga = true,
-    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-  },
-}
-
-require("tokyonight").setup {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  light_style = "day", -- The theme is used when the background is set to light
-  transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
-  },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-}
-
-vim.cmd.colorscheme "catppuccin"
--- vim.cmd.colorscheme "rose-pine"
+vim.cmd.colorscheme "rose-pine"
