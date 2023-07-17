e36d9d07a18fb5a75707df27b38e82d65f626a80
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-07-17 02:04:31 +0200

    wezterm: change font.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,19 +3,9 @@ local wezterm = require "wezterm"
 local colors = require("lua/rose-pine").colors()
 local window_frame = require("lua/rose-pine").window_frame()
 
--- Dynamically set theme based on OS appearance.
--- TODO: Use when dynamic theming solved for other utils.
-function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Catppuccin Frappe"
-  else
-    return "Catppuccin Latte"
-  end
-end
-
 return {
-  font = wezterm.font "FiraCode Nerd Font",
-  font_size = 11.0,
+  font = wezterm.font "BlexMono Nerd Font",
+  font_size = 12.0,
   freetype_load_flags = "NO_HINTING",
 
   -- Disable ligatures
@@ -23,8 +13,6 @@ return {
 
   enable_tab_bar = false,
   window_decorations = "RESIZE",
-  -- color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-  -- color_scheme = "Catppuccin Mocha",
   colors = colors,
   default_cursor_style = "BlinkingBlock",
   cursor_blink_ease_in = "Constant",
