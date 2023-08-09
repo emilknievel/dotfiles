9241c1588bf1c8bc2a3b0aa9e935493f363b2a0b
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-08-09 04:57:56 +0200

    vim: legacy tag for fidget.nvim, remove ignore  for null-ls

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -47,16 +47,18 @@ return {
 
   {
     "j-hui/fidget.nvim",
+    tag = "legacy",
+    event = "LspAttach",
     config = function()
       require("fidget").setup {
         text = {
           spinner = "clock", -- animation shown when tasks are ongoing
         },
-        sources = {
-          ["null-ls"] = {
-            ignore = true,
-          },
-        },
+        -- sources = {
+        --   ["null-ls"] = {
+        --     ignore = true,
+        --   },
+        -- },
       }
     end,
   },
