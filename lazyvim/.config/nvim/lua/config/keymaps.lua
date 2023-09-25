83c84ddf4d2be0f1c34a74e1595e93fef99628fb
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-09-25 18:49:26 +0200

    feat(vim): toggle theme with <leader>tt

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function toggle_theme()
+  if vim.o.background == "light" then
+    vim.cmd("set background=dark")
+  else
+    vim.cmd("set background=light")
+  end
+end
+
+vim.keymap.set("n", "<leader>tt", toggle_theme, { desc = "Toggle theme" })
