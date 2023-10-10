98c63145614840f3a1e1df53e54e95b678b4a5b3
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-10-10 06:05:11 +0200

    feat(emacs): use fixed-pitch font for org tables

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -303,7 +303,7 @@
  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
  '(org-property-value ((t (:inherit fixed-pitch))) t)
  '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- ;; '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-table ((t (:inherit (fixed-pitch org-table)))))
  '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
  '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
 
