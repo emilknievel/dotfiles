8875c3974715889d540f246d2900f31fc15eec33
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-10-10 06:05:53 +0200

    feat(emacs): show vc diff in dired buffer
    This makes it so that modified files in dired are highlighted in the
    gutter.

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -790,7 +790,8 @@ parses its input."
 (use-package diff-hl
   :init
   (global-diff-hl-mode)
-  (diff-hl-flydiff-mode)
+  (diff-hl-flydiff-mode) ; update diff-hl on the fly
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode) ; show diff in dired
   :hook
   (magit-pre-refresh . diff-hl-magit-pre-refresh)
   (magit-post-refresh . diff-hl-magit-post-refresh))
