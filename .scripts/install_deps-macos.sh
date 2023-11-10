3928ed70a376ce57505ae8c102a8cab3a124f6d3
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-11-10 15:59:41 +0100

    feat(make): homebrew on macos
    - Installs homebrew with make install_deps on macos.

--- .scripts/install_deps-macos.sh
+++ .scripts/install_deps-macos.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Not implemented"
+if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed!"
+fi
