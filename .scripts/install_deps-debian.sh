9786c05f6e75ebe838d0f78ee21857ebca4ee32c
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-11-11 16:44:09 +0100

    feat(window_management): bspwm, sxhkd, polybar
    BSPWM window manager with polybar, sxhkd and example configs.
    
    TODO: Adjust configurations for my system.

--- .scripts/install_deps-debian.sh
+++ .scripts/install_deps-debian.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 function install_nala() {
-    echo "Installing Nala..."
-    echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-    wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
-    sudo apt update
-    sudo apt install nala
-    if command -v nala &>/dev/null; then
-        echo "Nala has been installed!"
-        sudo nala fetch
+    if ! command -v nala &>/dev/null; then
+        echo "Installing Nala..."
+        echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+        wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+        sudo apt update
+        sudo apt install nala
+        if command -v nala &>/dev/null; then
+            echo "Nala has been installed!"
+            sudo nala fetch
+        fi
+    else
+        echo "Nala is already installed!"
     fi
 }
 
-if ! command -v nala &>/dev/null; then
-    install_nala
-else
-    echo "Nala is already installed!"
-fi
+function install_build_deps() {
+    echo "Installing build dependencies..."
+    sudo nala install -y build-essential cmake ninja-build
+}
+
+function install_bspwm_sxhkd_and_polybar() {
+    echo "Installing bspwm and sxhkd..."
+    sudo nala install -y bspwm
+    echo "Installing polybar..."
+    sudo nala install -y polybar
+}
+
+install_nala
+install_build_deps
+install_bspwm_sxhkd_and_polybar
