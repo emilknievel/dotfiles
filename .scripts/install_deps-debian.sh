d3afad3ce27a52d6a55e8fdf8302f06d20c2f06b
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-11-14 23:05:10 +0100

    feat(scripts): get rid of nala from deb script

--- .scripts/install_deps-debian.sh
+++ .scripts/install_deps-debian.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
-function install_nala() {
-    if ! command -v nala &>/dev/null; then
-        echo "Installing Nala..."
-        echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-        wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
-        sudo apt update
-        sudo apt install nala
-        if command -v nala &>/dev/null; then
-            echo "Nala has been installed!"
-            sudo nala fetch
-        fi
-    else
-        echo "Nala is already installed!"
-    fi
-}
-
 function install_build_deps() {
     echo "Installing build dependencies..."
-    sudo nala install -y build-essential cmake ninja-build
+    sudo apt install -y build-essential cmake ninja-build
 }
 
-function install_bspwm_sxhkd_and_polybar() {
+function install_window_manager() {
     echo "Installing bspwm and sxhkd..."
-    sudo nala install -y bspwm
+    sudo apt install -y bspwm
     echo "Installing polybar..."
-    sudo nala install -y polybar
-    sudo nala install -y rofi # launcher
+    sudo apt install -y polybar
+    sudo apt install -y rofi # launcher
 }
 
-install_nala
 install_build_deps
 install_bspwm_sxhkd_and_polybar
