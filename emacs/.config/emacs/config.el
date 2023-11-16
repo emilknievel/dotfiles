a2a0ee9c4d2b04edac6b5e87f1011204f815e4ff
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-11-16 15:33:17 +0100

    emacs: semi-transparent background

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -266,6 +266,8 @@
       mac-option-modifier nil
       mac-control-modifier 'control)
 
+(set-frame-parameter nil 'alpha-background 90)
+
 ;; (defvar ev/dark-theme 'doom-one)
 ;; (defvar ev/light-theme 'doom-one-light)
 (defvar ev/current-theme 'catppuccin)
