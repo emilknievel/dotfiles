2da1a6583a35d3667b741a3c081d2a4a98cfbfa7
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-11-28 22:04:24 +0100

    vim(surround): bring back default mappings
    - Disabled flash.nvim that collided with mini-surround.
    - Remapped mini-surround to be more like original vim mappings.

--- lazyvim/.config/nvim/lua/config/lazy.lua
+++ lazyvim/.config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { "folke/flash.nvim", enabled = false },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---
+++ lazyvim/.config/nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      -- use default vim surround mappings
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+      },
+    },
+  },
+}
