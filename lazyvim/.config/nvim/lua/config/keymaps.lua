aa1807fdff0e145be426e27ceffb282a41e83873
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-12-18 23:15:17 +0100

    shell: change handling of base16 colorscheme

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -2,15 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local function toggle_theme()
-  if vim.o.background == "light" then
-    vim.cmd("set background=dark")
-    vim.cmd("colorscheme catppuccin")
-  else
-    vim.cmd("set background=light")
-    vim.cmd("colorscheme tokyonight")
-  end
-end
-
-vim.keymap.set("n", "<leader>tt", toggle_theme, { desc = "Toggle theme" })
 vim.keymap.set("i", "jj", "<Esc>")
--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
+local base16_theme = os.getenv("BASE16_THEME")
+
 local colorscheme = function()
-  local base16_theme = os.getenv("BASE16_THEME")
-  local theme_name = base16_theme and base16_theme:match("tokyo%-night") and "tokyonight" or "catppuccin"
+  local theme_name = base16_theme and base16_theme:match("tokyo%-night") and "tokyonight"
+    or base16_theme and base16_theme:match("solarized") and "solarized"
+    or base16_theme and base16_theme:match("catppuccin") and "catppuccin"
+    or "gruvbox"
+
   return theme_name
 end
 
@@ -13,7 +18,9 @@ return {
     opts = {
       background = {
         light = "latte",
-        dark = "mocha",
+        dark = base16_theme and base16_theme:match("mocha") and "mocha" or base16_theme and base16_theme:match(
+          "macchiato"
+        ) and "macchiato" or base16_theme and base16_theme:match("frappe") and "frappe",
       },
       -- transparent_background = true,
     },
