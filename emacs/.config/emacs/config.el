9d8149e00277522aa33b692aef1fd27055411071
Emil Vågstedt <emil.vagstedt@icloud.com>
2023-12-30 12:32:58 +0100

    emacs(consult-dir): jumpt to directory with consult & eshell

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -928,6 +928,34 @@ parses its input."
   :after (treemacs nerd-icons)
   :config (treemacs-load-theme "nerd-icons"))
 
+(use-package consult-dir
+  :ensure t
+  :bind (("C-x C-d" . consult-dir)
+         :map vertico-map
+         ("C-x C-d" . consult-dir)
+         ("C-x C-j" . consult-dir-jump-file)))
+
+;; https://karthinks.com/software/jumping-directories-in-eshell/
+(defun eshell/z (&optional regexp)
+  "Navigate to a previously visited directory in eshell, or to
+any directory proferred by `consult-dir'."
+  (let ((eshell-dirs (delete-dups
+                      (mapcar 'abbreviate-file-name
+                              (ring-elements eshell-last-dir-ring)))))
+    (cond
+     ((and (not regexp) (featurep 'consult-dir))
+      (let* ((consult-dir--source-eshell `(:name "Eshell"
+                                                 :narrow ?e
+                                                 :category file
+                                                 :face consult-file
+                                                 :items ,eshell-dirs))
+             (consult-dir-sources (cons consult-dir--source-eshell
+                                        consult-dir-sources)))
+        (eshell/cd (substring-no-properties
+                    (consult-dir--pick "Switch directory: ")))))
+     (t (eshell/cd (if regexp (eshell-find-previous-directory regexp)
+                     (completing-read "cd: " eshell-dirs)))))))
+
 (use-package editorconfig
   :diminish
   :config (editorconfig-mode 1))
