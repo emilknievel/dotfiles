8cc527bb7e1969579b1c84c2317b4b6ec10d8d7c
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-02-10 18:28:58 +0100

    term(wezterm|alacritty): fira code nerd font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,17 +1,18 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("Iosevka Nerd Font"),
-	font_size = 14.0,
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 13.0,
 	-- freetype_load_flags = "NO_HINTING",
 
 	-- Disable ligatures
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 
-	enable_tab_bar = false,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
-	-- colors = colors,
-	-- color_scheme = "Rosé Pine (Gogh)",
+	color_scheme = "Tokyo Night",
 	default_cursor_style = "BlinkingBlock",
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
