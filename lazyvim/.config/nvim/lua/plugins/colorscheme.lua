4173e1dbbe6af65c0ad9e9448081ffd2a9f928f3
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-02-11 11:30:25 +0100

    nvim(theme): base16

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,55 +1,46 @@
 return {
+  -- {
+  --   "xiyaowong/nvim-transparent",
+  --   enabled = true,
+  --   opts = {
+  --     groups = { -- table: default groups
+  --       "Normal",
+  --       "NormalNC",
+  --       "Comment",
+  --       "Constant",
+  --       "Special",
+  --       "Identifier",
+  --       "Statement",
+  --       "PreProc",
+  --       "Type",
+  --       "Underlined",
+  --       "Todo",
+  --       "String",
+  --       "Function",
+  --       "Conditional",
+  --       "Repeat",
+  --       "Operator",
+  --       "Structure",
+  --       "LineNr",
+  --       "NonText",
+  --       "SignColumn",
+  --       "CursorLine",
+  --       "CursorLineNr",
+  --       "StatusLine",
+  --       "StatusLineNC",
+  --       "EndOfBuffer",
+  --     },
+  --     extra_groups = {}, -- table: additional groups that should be cleared
+  --     exclude_groups = {}, -- table: groups you don't want to clear
+  --   },
+  -- },
   {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      terminal_colors = true,
-      -- styles = {
-      --   floats = "dark",
-      -- },
-    },
-  },
-  {
-    "xiyaowong/nvim-transparent",
-    enabled = true,
-    opts = {
-      groups = { -- table: default groups
-        "Normal",
-        "NormalNC",
-        "Comment",
-        "Constant",
-        "Special",
-        "Identifier",
-        "Statement",
-        "PreProc",
-        "Type",
-        "Underlined",
-        "Todo",
-        "String",
-        "Function",
-        "Conditional",
-        "Repeat",
-        "Operator",
-        "Structure",
-        "LineNr",
-        "NonText",
-        "SignColumn",
-        "CursorLine",
-        "CursorLineNr",
-        "StatusLine",
-        "StatusLineNC",
-        "EndOfBuffer",
-      },
-      extra_groups = {}, -- table: additional groups that should be cleared
-      exclude_groups = {}, -- table: groups you don't want to clear
-    },
+    { "RRethy/base16-nvim" },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "base16-bright",
     },
   },
 }
