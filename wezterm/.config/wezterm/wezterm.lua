25f8f2ff02224e163b84ebee92697ad386b1832b
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-02-18 20:10:58 +0100

    term: alacritty + wezterm changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("FiraCode Nerd Font"),
+	font = wezterm.font("MesloLGS NF"),
 	font_size = 13.0,
-	-- freetype_load_flags = "NO_HINTING",
+	freetype_load_flags = "NO_HINTING",
 
 	-- Disable ligatures
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
@@ -11,11 +11,10 @@ return {
 	enable_tab_bar = true,
 	use_fancy_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
-	window_decorations = "RESIZE",
-	color_scheme = "Tokyo Night",
+	-- window_decorations = "RESIZE",
 	default_cursor_style = "BlinkingBlock",
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
 
-	front_end = "WebGpu",
+	-- front_end = "WebGpu",
 }
