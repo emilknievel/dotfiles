b79a7a9655818f2f95df26c5f40f30918eacd24a
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-03-23 12:38:22 +0100

    nvim(theme): gruvbox

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -36,9 +36,20 @@ return {
   },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      transparent_mode = true,
+      overrides = {
+        NotifyBackground = { bg = "#ff9900" },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox",
     },
   },
 }
