92e5c41a4cd8a6765e12c6f00573517a44c497bd
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-04-08 06:32:50 +0200

    emacs: ef themes

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -321,8 +321,8 @@
   :diminish
   :init
   (setq auto-dark-allow-osascript t) ; needed for it to work with emacsclient on macOS.
-  (setq auto-dark-dark-theme 'doom-solarized-dark
-        auto-dark-light-theme 'doom-solarized-light)
+  (setq auto-dark-dark-theme 'ef-dark
+        auto-dark-light-theme 'ef-light)
   :config
   (add-hook 'auto-dark-dark-mode-hook
             (lambda ()
