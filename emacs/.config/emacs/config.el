bc37ea3db0eb35d2e09dc208022d9443d35c4e6c
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-05-07 14:59:42 +0200

    emacs(macos): iosevka font

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -367,7 +367,7 @@
   (auto-dark-mode t))
 
 (defvar ev-linux-font "Source Code Pro")
-(defvar ev-macos-font "CaskaydiaCove Nerd Font")
+(defvar ev-macos-font "Iosevka")
 ;; (defvar ev-heading-font "Iosevka Aile")
 
 (if (eq system-type 'darwin)
@@ -412,11 +412,11 @@
           ev-variable-pitch-font-weight 'normal
           ev-variable-pitch-font-width 'normal
 
-          ev-editor-font-height 130
+          ev-editor-font-height 140
           ev-editor-font-weight 'normal
           ev-editor-font-width 'normal
 
-          ev-fixed-pitch-font-height 130
+          ev-fixed-pitch-font-height 140
           ev-fixed-pitch-font-weight 'normal
           ev-fixed-pitch-font-width 'normal)
   (ev-setup-linux-fonts))
