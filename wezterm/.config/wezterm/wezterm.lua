07e63486b5fa5916f10d96674b068273049f53a4
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-06-03 22:58:24 +0200

    term: wezterm macos config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,36 +1,35 @@
-local wezterm = require("wezterm")
-
--- wezterm.gui is not available to the mux server, so take care to
--- do something reasonable when this config is evaluated by the mux
-local function get_appearance()
-   if wezterm.gui then
-      return wezterm.gui.get_appearance()
-   end
-   return "Dark"
-end
+local wezterm = require('wezterm')
 
 local function scheme_for_appearance(appearance)
-   if appearance:find("Dark") then
-      return "Dark"
-   else
-      return "Terminal Basic"
-   end
+  if appearance:find('Dark') then
+    return 'Dark'
+  else
+    return 'Bluloco Zsh Light (Gogh)'
+  end
 end
 
+wezterm.on('window-config-reloaded', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
 return {
-   -- temporarily disable wayland until new nvidia stuff is out
-   enable_wayland = false,
-   color_scheme = scheme_for_appearance(get_appearance()),
-   -- window_background_opacity = 0.9,
-   font = wezterm.font("Noto Sans Mono"),
-   font_size = 10.0,
+  window_background_opacity = 1.0,
+  font = wezterm.font('SFMono Nerd Font'),
+  font_size = 13.0,
+  freetype_load_target = 'Light',
+  freetype_load_flags = 'NO_HINTING',
 
-   -- Disable ligatures
-   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
-   enable_tab_bar = true,
-   use_fancy_tab_bar = false,
-   hide_tab_bar_if_only_one_tab = true,
+  enable_tab_bar = true,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
 
-   window_decorations = "RESIZE"
+  window_decorations = 'TITLE | RESIZE',
 }
