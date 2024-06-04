0d5352cf399f5a2f3f6800de6469e7e6a3d286fc
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-06-04 23:02:01 +0200

    term(linux): wezterm look and feel

--- wezterm-linux/.config/wezterm/wezterm.lua
+++ wezterm-linux/.config/wezterm/wezterm.lua
@@ -16,16 +16,25 @@ local function scheme_for_appearance(appearance)
 end
 
 return {
+  -- Graphics
   enable_wayland = false,
   color_scheme = scheme_for_appearance(get_appearance()),
 
+  -- Font features
   font = wezterm.font('NotoSansM Nerd Font'),
   font_size = 10.0,
+  adjust_window_size_when_changing_font_size = false,
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
+  -- Windows
+  initial_cols = 128,
+  initial_rows = 32,
+
+  -- Tabs
   enable_tab_bar = true,
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
 
+  -- Misc
   scrollback_lines = 50000,
 }
