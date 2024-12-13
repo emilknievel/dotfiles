a7bdd48242539c82fa3db963eaf7fd528d0d7dfe
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-12-13 14:20:21 +0100

    emacs(editing): nerd icons + corfu, delete selected text if writing

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -238,6 +238,10 @@
     ("M-d" mc/mark-next-like-this "mark next")
     ("M-D" mc/unmark-previous-like-this "mark previous")))
 
+(use-package delsel
+  :ensure nil ; no need to install it as it is built-in
+  :hook (after-init . delete-selection-mode))
+
 (use-package eldoc-box
   :config
   (add-hook 'eglot-managed-mode-hook #'eldoc-box-hover-mode t))
@@ -900,6 +904,12 @@ parses its input."
 
   (setq tab-always-indent t))
 
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
 ;; Add extensions
 (use-package cape
   ;; Bind dedicated completion commands
@@ -984,53 +994,12 @@ parses its input."
   :after eglot
   :config (eglot-booster-mode))
 
-(setq treesit-language-source-alist
-      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-        (cmake "https://github.com/uyha/tree-sitter-cmake")
-        (css "https://github.com/tree-sitter/tree-sitter-css")
-        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-        (go "https://github.com/tree-sitter/tree-sitter-go")
-        (html "https://github.com/tree-sitter/tree-sitter-html")
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-        (json "https://github.com/tree-sitter/tree-sitter-json")
-        (lua "https://github.com/tree-sitter-grammars/tree-sitter-lua")
-        (make "https://github.com/alemuller/tree-sitter-make")
-        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-        (python "https://github.com/tree-sitter/tree-sitter-python")
-        (toml "https://github.com/tree-sitter/tree-sitter-toml")
-        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-        (yaml "https://github.com/ikatyang/tree-sitter-yaml")
-        (ocaml "https://github.com/tree-sitter/tree-sitter-ocaml" "master" "grammars/ocaml/src")
-        (c-sharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
-        (c "https://github.com/tree-sitter/tree-sitter-c")
-        (cpp "https://github.com/tree-sitter/tree-sitter-cpp/" "master" "src")))
-
-(defun my-install-ts-grammars ()
-  "Install grammars for tree-sitter from treesit-language-source-alist."
-  (interactive)
-  (mapc #'treesit-install-language-grammar
-        (mapcar #'car treesit-language-source-alist)))
-
-(setq major-mode-remap-alist
-      '((bash-mode . bash-ts-mode)
-        (cmake-mode . cmake-ts-mode)
-        (css-mode . css-ts-mode)
-        (elisp-mode . elisp-ts-mode)
-        (go-mode . go-ts-mode)
-        (html-mode . html-ts-mode)
-        (javascript-mode . js-ts-mode)
-        (json-mode . json-ts-mode)
-        (make-mode . make-ts-mode)
-        (python-mode . python-ts-mode)
-        (toml-mode . toml-ts-mode)
-        (tsx-mode . tsx-ts-mode)
-        (typescript-mode . typescript-ts-mode)
-        (yaml-mode . yaml-ts-mode)
-        (ocaml-mode . ocaml-ts-mode)
-        ;; (csharp-mode . csharp-ts-mode)
-        (c-mode . c-ts-mode)
-        (c++-mode . c++-ts-mode)))
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
 
 (add-to-list 'auto-mode-alist '("\\.pl?\\'" . prolog-mode))
 
