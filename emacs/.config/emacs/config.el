8eae73d1b07853e8354f1dfc1ee94868bb80ee57
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-12-21 18:04:02 +0100

    emacs(macOS): menlo fixed pitch font

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -503,7 +503,7 @@ bar not using the proper theme if the server was loaded with a different theme."
   (ef-themes-select 'ef-winter))
 
 (defvar my-linux-font "CaskaydiaCove Nerd Font")
-(defvar my-macos-font "SF Mono")
+(defvar my-macos-font "Menlo")
 
 (if (eq system-type 'darwin)
     (defvar my-editor-font my-macos-font)
