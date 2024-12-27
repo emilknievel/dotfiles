91096e846854ba1dbe1b9402376fd043a79ff589
Emil Vågstedt <emil.vagstedt@icloud.com>
2024-12-27 22:56:27 +0100

    wezterm: padding, font, color theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,7 +4,8 @@ local function scheme_for_appearance(appearance)
   if appearance:find('Dark') then
     return 'Dark'
   else
-    return 'Modus-Operandi'
+    -- return 'Modus-Operandi'
+    return 'Catppuccin Latte'
   end
 end
 
@@ -21,7 +22,7 @@ end)
 return {
   -- window_background_opacity = 0.90,
   -- macos_window_background_blur = 20,
-  font = wezterm.font('SF Mono'),
+  font = wezterm.font('Menlo'),
   font_size = 13.0,
 
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
@@ -30,7 +31,17 @@ return {
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
 
-  window_decorations = 'RESIZE',
+  window_decorations = 'TITLE | RESIZE',
 
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+
+  use_resize_increments = true,
+  initial_cols = 80,
+  initial_rows = 24,
   scrollback_lines = 5000,
 }
