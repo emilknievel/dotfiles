3d4b78b8fc2d1ade23e70d84ce2a5ca9abef173f
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-01-14 23:26:39 +0100

    emacs(movement): add global binding for avy
    `C-:` to make a search based jump in the buffer.

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -1432,6 +1432,8 @@ any directory proferred by `consult-dir'."
   :config
   (global-anzu-mode))
 
+(global-set-key (kbd "C-:") 'avy-goto-char-timer)
+
 (use-package org
   :init
   (setq org-directory (expand-file-name "~/Documents/org")
