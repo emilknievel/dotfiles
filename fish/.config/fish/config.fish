c1847c4fa63d55b53b058e0f777d362cb5b13620
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-01-25 17:35:30 +0100

    fish: hooks for zoxide, direnv mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+zoxide init fish | source
+direnv hook fish | source
+~/.local/bin/mise activate fish | source
