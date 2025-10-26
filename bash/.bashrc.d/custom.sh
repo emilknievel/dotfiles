b73f59307dd6b8ea7272c3bfb1f4841acd11727e
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-10-26 21:40:10 +0100

    shell: custom functions

---
+++ bash/.bashrc.d/custom.sh
@@ -0,0 +1,28 @@
+copy() {
+  if hash pbcopy 2>/dev/null; then
+    exec pbcopy
+  elif hash xclip 2>/dev/null; then
+    exec xclip -selection clipboard
+  elif hash putclip 2>/dev/null; then
+    exec putclip
+  else
+    rm -f /tmp/clipboard 2> /dev/null
+    if [ $# -eq 0 ]; then
+      cat > /tmp/clipboard
+    else
+      cat "$1" > /tmp/clipboard
+    fi
+  fi
+}
+
+pasta() {
+  if hash pbpaste 2>/dev/null; then
+    exec pbpaste
+  elif hash xclip 2>/dev/null; then
+    exec xclip -selection clipboard -o
+  elif [[ -e /tmp/clipboard ]]; then
+    exec cat /tmp/clipboard
+  else
+    echo ''
+  fi
+}
