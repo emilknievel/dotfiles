2d6e0ab52f496703966be7b5efccb6dbada7a182
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-11-28 05:55:44 +0100

    emacs: Reorder + add excluded modes for trailing whitespace

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -331,18 +331,20 @@ Example usage: \(get-auth-keyword \"test\" :secret)"
 (require 'whitespace)
 (setq-default show-trailing-whitespace t)
 
-(dolist (hook '(shell-mode-hook
-                eshell-mode-hook
-                term-mode-hook
-                vterm-mode-hook
-                eat-mode-hook
-                compilation-mode-hook
+(dolist (hook '(
+                Buffer-menu-mode-hook
                 Info-mode-hook
                 calendar-mode-hook
-                elpaca-log-mode-hook
+                compilation-mode-hook
                 devdocs-mode-hook
+                eat-mode-hook
+                elpaca-log-mode-hook
+                eshell-mode-hook
                 eww-mode-hook
                 modus-themes-preview-mode-hook
+                shell-mode-hook
+                term-mode-hook
+                vterm-mode-hook
                 ))
   (add-hook hook (lambda ()
                    (setq show-trailing-whitespace nil))))
