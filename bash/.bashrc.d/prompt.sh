4336e99805d74eaa9f02f8364b27a490e8057d02
Emil Vågstedt <emil.vagstedt@icloud.com>
2025-12-13 22:08:35 +0100

    bash: Prompt adjustments
    Move to manual config instead of external Starship tool.

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -1,3 +1,5 @@
+## Prompt configuration
+
 # List number of session jobs in prompt
 jobs_count() {
     local job_count
@@ -9,11 +11,7 @@ jobs_count() {
     fi
 }
 
-if command -v starship &> /dev/null; then
-    eval "$(starship init bash)"
-else
-    export PROMPT_DIRTRIM=1
-    export PROMPT_COMMAND='history -a; JOB_COUNT=$(jobs_count)'
-    # shellcheck disable=SC2153
-    export PS1='\[\e[32m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[0m\]${JOB_COUNT}\$ '
-fi
+export PROMPT_DIRTRIM=1
+export PROMPT_COMMAND='history -a; JOB_COUNT=$(jobs_count)'
+# shellcheck disable=SC2153
+export PS1='\[\e[32m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[0m\]${JOB_COUNT}\$ '
