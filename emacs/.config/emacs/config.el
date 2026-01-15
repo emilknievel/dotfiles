476408d44e32b1dbb9b349e3a23005f3720aa197
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-01-15 20:47:30 +0100

    emacs: org journal daily template

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -2181,7 +2181,7 @@ With two prefix arguments, insert as top-level heading."
     (add-to-list 'org-capture-templates
                  '("ndjd" "Daily" entry
                    (file denote-journal-path-to-new-or-existing-entry)
-                   "* %U\n%?"
+                   "* %U\n\n%?"
                    :kill-buffer t
                    :empty-lines 1))
     (add-to-list 'org-capture-templates
