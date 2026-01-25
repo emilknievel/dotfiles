1e50d453da3b5b332b6d353b8df1a09052540b25
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-01-25 19:05:32 +0100

    emacs: change default frame size

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -40,7 +40,7 @@
 (setq package-enable-at-startup nil)
 
 ;; Customize titlebar
-(setq default-frame-alist '((width . 100) (height . 45)))
+(setq default-frame-alist '((width . 110) (height . 35)))
 (cond ((eq system-type 'darwin)
        ;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
        (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t)))
