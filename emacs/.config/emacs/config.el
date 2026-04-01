c216a98909e280e743cfcc697fdf3b10c0a58792
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-04-01 19:32:43 +0200

    emacs: remove ocaml config

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -1387,20 +1387,6 @@ This command requires that pandoc (man page `pandoc(1)') be installed."
   (add-to-list 'project-vc-extra-root-markers ".busted")
   :hook (lua-mode . eglot-ensure))
 
-(use-package tuareg :ensure t)
-
-;; OCaml configuration
-;;  - better error and backtrace matching
-
-(defun set-ocaml-error-regexp ()
-  (set
-   'compilation-error-regexp-alist
-   (list '("[Ff]ile \\(\"\\(.*?\\)\", line \\(-?[0-9]+\\)\\(, characters \\(-?[0-9]+\\)-\\([0-9]+\\)\\)?\\)\\(:\n\\(\\(Warning .*?\\)\\|\\(Error\\)\\):\\)?"
-           2 3 (5 . 6) (9 . 11) 1 (8 compilation-message-face)))))
-
-(add-hook 'tuareg-mode-hook 'set-ocaml-error-regexp)
-(add-hook 'caml-mode-hook 'set-ocaml-error-regexp)
-
 (use-package dockerfile-mode :ensure t)
 
 (use-package csv-mode
