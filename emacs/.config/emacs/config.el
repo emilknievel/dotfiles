c1817bfb37f53bc9492587da6d284c83672372b9
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-04-08 17:09:52 +0200

    emacs: new name for batppuccin theme

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -428,11 +428,11 @@ loaded with a different theme."
 
 (use-package standard-themes :ensure t)
 
-(use-package batppuccin-themes
+(use-package batppuccin
   :ensure (:host "www.github.com"
                  :repo "bbatsov/batppuccin-emacs"
                  :branch "main"
-                 :main "batppuccin-themes.el")
+                 :main "batppuccin.el")
   :init
   (defun my-batppuccin-mocha ()
     (interactive)
