46a97c2a86689df58bcb7ae140aa3a67c8db3168
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-04-22 23:32:38 +0200

    bash: make prompt failures easier to spot
    Keep successful prompts visually quiet while making failures
    stand out more clearly.
    
    This makes the shell state easier to scan at a glance without
    adding noise to the common case.

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -3,11 +3,11 @@ error_display() {
 	if [ $exit_status -eq 0 ]; then
 		echo ""
 	else
-		echo "[$exit_status]"
+		echo "($exit_status) "
 	fi
 }
 
 export PROMPT_DIRTRIM=1
 export PROMPT_COMMAND='ERROR_DISPLAY=$(error_display $?); history -a'
 # shellcheck disable=SC2153
-export PS1='\[\e[32;1m\]\u@\h\[\e[0m\]:\[\e[34;1m\]\w\[\e[0m\]\$\[\e[31m\]$ERROR_DISPLAY\[\e[0m\] '
+export PS1='\[\e[31m\]$ERROR_DISPLAY\[\e[0m\]\[\e[32;1m\]\u@\h\[\e[0m\] \[\e[34;1m\]\w\[\e[0m\] \$ '
