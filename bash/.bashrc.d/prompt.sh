4079f7580993573ce0ea8efa27dcf55562eb9b99
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-04-23 10:08:24 +0200

    shell: show more context in compact paths

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -7,7 +7,7 @@ error_display() {
 	fi
 }
 
-export PROMPT_DIRTRIM=1
+export PROMPT_DIRTRIM=2
 export PROMPT_COMMAND='ERROR_DISPLAY=$(error_display $?); history -a'
 # shellcheck disable=SC2153
 export PS1='\[\e[31m\]$ERROR_DISPLAY\[\e[0m\]\[\e[32;1m\]\u@\h\[\e[0m\] \[\e[34;1m\]\w\[\e[0m\] \$ '
