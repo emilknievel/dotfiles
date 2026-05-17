d4c62cf28978b62eb9610c602e3ccfa524d159c2
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-05-17 18:12:21 +0200

    meta: remove stale install scripts
    The install scripts have not been looked at for quite some time and have
    become stale. This work will be something to look at in the future,
    because I like the idea of just pressing a button and have everything
    configured from scratch.

--- .scripts/install_deps-macos.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v brew &>/dev/null; then
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Homebrew is already installed!"
-fi
