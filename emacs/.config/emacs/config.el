765831c4fe77d9822b157c21a19b3b5182d537f4
Emil Vågstedt <emil.vagstedt@icloud.com>
2026-06-30 20:15:19 +0200

    emacs(org): unhide emphasis markers

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -2371,8 +2371,8 @@ This command requires that pandoc (man page `pandoc(1)') be installed."
   :ensure t
   :after general
   :config
-  (setopt org-hide-emphasis-markers t
-          org-appear-autoemphasis t
+  (setopt org-hide-emphasis-markers nil
+          org-appear-autoemphasis nil
           org-appear-autolinks t
           org-appear-autosubmarkers t
           org-appear-autoentities t
